e022791b0ddedcec465301fafce95ef5a5d92812
Nishan <nishan.bende@solutelabs.com>
2018-08-14 16:25:47 +0530

    Fixed failing testCases

---
+++ test/get-value.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var test = require('tap').test
+var countries = require('../')()
+
+test('get value from country name', function (t) {
+  t.equal(countries.getValue('Iceland'), 'IS', 'name "Iceland" should return IS')
+  t.equal(countries.getValue('iceland'), 'IS', 'name "iceland" should return IS')
+  t.type(countries.getValue('iceland'), 'string', 'type of name "IS" is string')
+  t.type(countries.getValue('IS'), 'undefined', 'name "IS" should return undefined')
+  t.type(countries.getValue('XX'), 'undefined', 'name "XX" should return undefined')
+  t.type(countries.getValue(''), 'undefined', 'empty name should return undefined')
+  t.end()
+})
